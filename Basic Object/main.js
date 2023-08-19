48f5ed4d60189ad2d809d20d55e1e871a52a05c9
Rahul Baskey <86495244+RahulBRB@users.noreply.github.com>
2023-08-19 11:22:58 +0530

    Create main.js

---
+++ Basic Object/main.js
@@ -0,0 +1,13 @@
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30,
+  isStudent: false,
+  sayHello: function() {
+    console.log(`Hello, my name is ${this.firstName} ${this.lastName}.`);
+  }
+};
+
+console.log(person.firstName); // Output: John
+console.log(person.age);       // Output: 30
+person.sayHello();             // Output: Hello, my name is John Doe.
