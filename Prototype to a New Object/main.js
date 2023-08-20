97ace04343c146315023b720bd0fe18f36f9c3ca
Rahul Baskey <86495244+RahulBRB@users.noreply.github.com>
2023-08-20 13:02:26 +0530

    Create main.js

---
+++ Prototype to a New Object/main.js
@@ -0,0 +1,18 @@
+/*
+A more efficient way is to set the prototype to a new object that already contains the properties. This way, the properties are added all at once:
+*/
+
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype = {
+  numLegs: 2,
+  eat: function(){
+    console.log("nom nom nom");
+  },
+
+  describe: function(){
+    console.log("My name is " + this.name);
+  }
+};
